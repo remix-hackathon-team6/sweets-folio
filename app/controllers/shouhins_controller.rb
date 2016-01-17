e583c1ea2a6c9ee82583aad77d4c5e728d19e090
atsumine <atsu9615@me.com>
2016-01-17 12:18:10 +0900

    店舗情報などを表示出来るように

--- app/controllers/shouhins_controller.rb
+++ app/controllers/shouhins_controller.rb
@@ -1,5 +1,7 @@
 class ShouhinsController < ApplicationController
   def show
 	@shouhin = Shouhin.find(params[:id])
+	@cook = User.find_by(shouhin_id: @shouhin.id);
+	@rel_shouhins = Shouhin.where(user_id: @cook.id);
   end
 end
