ce65969e3224d172ebe91386e5b1be8ae8bf19f8
atsumine <atsu9615@me.com>
2016-01-17 15:19:09 +0900

    コメントアウト

--- app/controllers/shouhins_controller.rb
+++ app/controllers/shouhins_controller.rb
@@ -4,5 +4,6 @@ class ShouhinsController < ApplicationController
     @cook = User.find_by(shouhin_id: @shouhin.id)
     @rel_shouhins = Shouhin.where(user_id: @cook.id)
     @comments = Eval.where(shouhin_id_id: @shouhin.id)
+    # @reviewer = User.where(id: @comments.user_id_id)
   end
 end
