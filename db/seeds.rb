0cc2833a5b80a3916eaa10b95560ddd61a3b6775
しぇぱっち <shepabashi@gmail.com>
2016-01-17 10:10:08 +0900

    シードを書き換えました

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(user_name: '田中太郎', email: 'tanaka@hoge.jp', user_image: 'tanaka.jpg', user_info: 'ほげほげ', shouhin_id: '1', pastry: true )
+User.create(user_name: 'めぐみさん', email: 'megumi@hoge.jp', user_image: 'megumi.jpg', user_info: '7歳のときからパティシエになるのが夢でした。腕には絶対的の地震があります♡', shouhin_id: '1', pastry: true )
 User.create(user_name: '山田一郎', email: 'yamada@huga.jp', user_image: 'yamada.jpg', user_info: 'ほげ-ほげ', shouhin_id: '2', pastry: false )
+User.create(user_name: '渡辺陽一', email: 'watanabe@huga.jp', user_image: 'watanabe.jpg', user_info: 'スイーツには目が無いです', shouhin_id: '3', pastry: false )
 
 Shouhin.create(shouhin_name: 'さくらんぼのショートケーキ', shouhin_info: '素材にこだわりぬいた本当にこだわりのケーキです。', shouhin_eval: '4', user_id: '1',
 	           shouhin_image_1: 'cake.jpg', shouhin_image_2: 'cake.jpg', shouhin_image_3: 'cake.jpeg', shouhin_image_4: 'cake.jpeg')
