445be36c0ec70c5deed076087ab4f6649322d55d
atsumine <atsu9615@me.com>
2016-01-17 13:48:32 +0900

    ある程度の処理が出来るように

--- app/controllers/shouhins_controller.rb
+++ app/controllers/shouhins_controller.rb
@@ -1,7 +1,8 @@
 class ShouhinsController < ApplicationController
   def show
-	@shouhin = Shouhin.find(params[:id])
-	@cook = User.find_by(shouhin_id: @shouhin.id);
-	@rel_shouhins = Shouhin.where(user_id: @cook.id);
+    @shouhin = Shouhin.find(params[:id])
+    @cook = User.find_by(shouhin_id: @shouhin.id)
+    @rel_shouhins = Shouhin.where(user_id: @cook.id)
+    @comments = Eval.where(shouhin_id_id: @shouhin.id)
   end
 end
